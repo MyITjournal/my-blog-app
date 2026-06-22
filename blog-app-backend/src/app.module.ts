7c03a373503cac36b4b3dc055824271d51dc8f77
MyITjournal <adesitjourney@gmail.com>
2026-06-22 14:39:56 +0100

    feat: import the search module

--- blog-app-backend/src/app.module.ts
+++ blog-app-backend/src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { AuthModule } from './modules/auth/auth.module.js';
-import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard.js';
-import { UsersModule } from './modules/users/users.module.js';
-import { PostsModule } from './modules/posts/posts.module.js';
-import { CommentsModule } from './modules/comments/comments.module.js';
-import { CategoriesModule } from './modules/categories/categories.module.js';
-import { TagsModule } from './modules/tags/tags.module.js';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
-import { PrismaModule } from './prisma/prisma.module.js';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { TagsModule } from './modules/tags/tags.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { SearchModule } from './modules/search/search.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { PrismaModule } from './prisma/prisma.module.js';
     CategoriesModule,
     TagsModule,
     AuthModule,
+    SearchModule,
   ],
   controllers: [AppController],
   providers: [
