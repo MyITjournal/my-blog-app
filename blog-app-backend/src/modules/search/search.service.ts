38495f2d15baff7adc9b1b3bac838b102a1bce3e
MyITjournal <adesitjourney@gmail.com>
2026-06-22 18:00:39 +0100

    fix: remove unnecessary imports

--- blog-app-backend/src/modules/search/search.service.ts
+++ blog-app-backend/src/modules/search/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { SearchDto } from './dto/search.dto.js';
+import { SearchDto } from './dto/search.dto';
 
 @Injectable()
 export class SearchService {
